2f232e63737dfc29bcb3f7946323e70481c7d686
Romeo Violini <romeo.violini@gmail.com>
2024-02-13 11:47:26 +0100

    added settings for constants

---
+++ settings.py
@@ -0,0 +1,4 @@
+MAX_CHARACTERS = 10000000  # Example limit for maximum characters allowed
+ALLOWED_FORMATS = ['.docx','.txt','.doc',]  # List of allowed file formats, extendable for future formats
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
