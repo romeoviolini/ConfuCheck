2cf23ce9fa0aba1bf531bbf22679699309fcf0ee
Romeo Violini <romeo.violini@gmail.com>
2024-02-17 19:48:32 +0100

    added constants for colors

--- settings.py
+++ settings.py
@@ -3,7 +3,9 @@ ALLOWED_FORMATS = ['.docx','.txt','.doc',]  # List of allowed file formats, exte
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 768
 FONT_SIZE = 14
-
+SELECTED_COLOR = "#f96d00"
+UNSELECTED_COLOR = "#000000"
+BACKGROUND_COLOR = "#faebcd"
 
 def formatTextAsHTML(text):
     #font_stack = "system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Ubuntu, 'Helvetica Neue', sans-serif"
